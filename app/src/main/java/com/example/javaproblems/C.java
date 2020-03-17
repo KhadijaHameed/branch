ff6e14fa83aa050859d92547595af244cecac93d
uhfsolution <uhfsolution@gmail.com>
2020-03-17 10:15:05 +0500

    initial commit

---
+++ app/src/main/java/com/example/javaproblems/C.java
@@ -0,0 +1,26 @@
+package com.example.javaproblems;
+
+import android.view.View;
+import android.widget.Toast;
+
+public class C {
+
+   /*  btnCheck.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String input = etORiginal.getText().toString();
+
+            int length = input.length();
+            for ( int i =  length- 1; i >= 0; i-- )
+                reverse = reverse + input.charAt(i);
+            if (input.equals(reverse)){
+                Toast.makeText(MainActivity.this, ":)string/number is a palindrome.", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(MainActivity.this, "@.@string/number IS NOT  a palindrome.", Toast.LENGTH_SHORT).show();
+            }
+
+            Toast.makeText(MainActivity.this, "input "+input, Toast.LENGTH_SHORT).show();
+        }
+    });
+*/
+}
---
+++ app/src/main/java/com/example/javaproblems/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.javaproblems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etORiginal, etReverse ;
+    String original, reverse = ""; // Objects of String class
+    Button btnCheck ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etORiginal = findViewById(R.id.editText);
+        btnCheck = findViewById(R.id.btn_check);
+
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = etORiginal.getText().toString();
+
+                int length = input.length();
+                for ( int i =length- 1; i >= 0; i-- )
+                    reverse = reverse + input.charAt(i);
+
+                if (input.equals(reverse)){
+                    Toast.makeText(MainActivity.this, "Entered string/number is a palindrome.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "orginial "+input+" reverse "+reverse, Toast.LENGTH_SHORT).show();
+                    etORiginal.setText("");
+                    reverse = "";
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "orginial "+input+" reverse "+reverse, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, " noooot  a palindrome.", Toast.LENGTH_SHORT).show();
+                    etORiginal.setText("");
+                    reverse = "";
+                }
+
+            }
+        });
+
+    }
+}
+
